644bfc20f0683fa5159fe6c74f7b65e7c3a96fc1
Michael Gravina <gravina.2@osu.edu>
2021-12-02 12:18:28 -0500

    Added easter egg

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,17 @@
+<script>
+    const catPic = `
+                                                     /\\__/\\   Genie is Great!!!
+                                                    ( ^ .^ )  /
+                                                     |        |  _
+                                                    /         \\ ))
+                                                   (  | | | |  )/
+                                                     (..)(..)
+`
+    function displaySecret() {
+        alert(`\n${catPic}\n\n`);
+    }
+</script>
+
 <main>
     <h1>ABOUT</h1>
 
@@ -6,10 +20,15 @@
     <div>
         This is Mike Gravina's attempt at building a domain-general, high-dimensional semantic medium.
     </div>
+
+    <div class="secret" on:click={displaySecret}>
+        ?
+    </div>
 </main>
   
 <style>
     main {
+        border-radius: 5px;
         box-shadow: 5px 5px 10px 10px lightgray;
 
         margin: 5rem;
@@ -17,6 +36,29 @@
         padding: 2rem;
 
         font-size: 1rem;
+    }
+
+    .secret {
+        border-radius: 5px;
+
+        position: absolute;
+        bottom: 25px;
+        right: 25px;
+
+        width: 25px;
+        height: 25px;
+
+        color: white;
+
+        cursor: pointer;
+    }
+
+    .secret:hover {
+        box-shadow: 5px 5px 10px 10px lightgray;
+
+        text-align: center;
+        line-height: 25px;
+        color: grey;
     }
 </style>
   
