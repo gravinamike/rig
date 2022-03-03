d167215f6b4e2386ae691a0c96d01767a35f5965
Michael Gravina <gravina.2@osu.edu>
2022-03-03 11:00:28 -0500

    Added double-click/click-out/escape functionality to start/end Notes editing

--- src/lib/viewers/notesViewers/notesEditor.svelte
+++ src/lib/viewers/notesViewers/notesEditor.svelte
@@ -2,64 +2,74 @@
     import { onMount, onDestroy } from "svelte"
     import { Editor } from "@tiptap/core"
     import StarterKit from "@tiptap/starter-kit"
-    import Underline from "@tiptap/extension-underline"
     import TextStyle from '@tiptap/extension-text-style'
-    import { FontSize } from "./extension-font-size"
     import FontFamily from "@tiptap/extension-font-family"
+    import { FontSize } from "./extension-font-size"
+    import Underline from "@tiptap/extension-underline"
     import NotesToolbar from "./notesToolbar.svelte"
 
-    export let notesText: string
+    export let noteText: string
 
 
     let textField: Element
     let editor: Editor
 
-    $: setContent(notesText)
+    $: setContent(noteText)
 
     function setContent(content: string) {
-        editor?.commands.setContent(content)
-    }
-
-    onMount(() => {
+        editor?.destroy()
         editor = new Editor({
             element: textField,
             extensions: [
                 StarterKit,
-                Underline,
                 TextStyle,
                 FontSize,
-                FontFamily
+                FontFamily,
+                Underline
             ],
-            content: '<p>Starting text...</p>',
+            content: content,
+            autofocus: true,
             onTransaction: () => {
                 editor = editor // Force re-render so `editor.isActive` works correctly.
             },
         })
-        setContent(notesText)
+        editor.commands.setContent(content)
+    }
+
+    onMount(() => {
+        setContent(noteText)
     })
 
     onDestroy(() => {
         if (editor) editor.destroy()
     })
+
+    function focusEditor() {
+        const editorElement = editor.view.dom as HTMLElement
+        if (editorElement !== document.activeElement) editorElement.focus()
+    }
 </script>
 
 
-<main>
-    <div class="text-container">
-        <div class="text-field"
-            bind:this={textField}
-        />
+<div class="notes-editor">
+    <!-- Editor text field. -->
+    <div
+        class="text-field"
+        bind:this={textField}
+        on:click|preventDefault={focusEditor}
+    >
     </div>
 
+    <!-- Editor toolbar. -->
     <NotesToolbar
         {editor}
     />
-</main>
+</div>
 
 
 <style>
-    main {
-        flex: 1 1;
+    .notes-editor {
+        flex: 1 1 0;
 
         background-color: #fafafa;
 
@@ -70,10 +80,10 @@
         text-align: center;
     }
 
-    .text-container {
+    .text-field {
         flex: 1 1 0;
 
-        outline: solid 1px lightgrey;
+        outline: solid 1px black;
         outline-offset: -1px;
 
         overflow-x: hidden;
@@ -81,6 +91,13 @@
 
         background-color: white;
 
+        padding: 0.5rem;
+
         text-align: left;
+        
+    }
+
+    :global(.ProseMirror:focus) {
+        outline: none;
     }
 </style>
--- src/lib/viewers/notesViewers/notesViewer.svelte
+++ src/lib/viewers/notesViewers/notesViewer.svelte
@@ -2,15 +2,16 @@
     import type { Graph } from "$lib/models/graphModels"
     import type { Thing } from "$lib/models/dbModels"
     import { thingsStore, storeGraphConstructs, retrieveGraphConstructs, graphConstructInStore } from "$lib/stores/graphStores"
-    import { Toggle } from "$lib/widgets/layoutWidgets"
     import NotesEditor from "./notesEditor.svelte"
     import { addNoteToThing } from "$lib/db/clientSide"
 
     export let graph: Graph
 
     
-    let editable: false
+    let notesContainer: Element
+    let editable = false
 
+    // Get Perspective Thing.
     let pThingIds: number[] | null = null
     $: pThingIds = graph.pThingIds
     $: pThingId = pThingIds && pThingIds.length ? pThingIds[0] : null
@@ -18,8 +19,9 @@
         retrieveGraphConstructs<Thing>("Thing", pThingId) :
         null
 
+    // Get Note title and text.
     $: title = pThing ? pThing.text : "THING NOT FOUND IN STORE"
-    $: notesText = pThing && pThing.note ? pThing.note.text : "NO NOTES YET"
+    $: noteText = pThing && pThing.note ? pThing.note.text : "NO NOTES YET"
 
     async function addNote() {///////////// This should only be called prior to saving newly entered Notes for a Thing that doesn't yet have Notes.
         if (pThing && !pThing.note) {
@@ -28,10 +30,26 @@
             await storeGraphConstructs<Thing>("Thing", pThing.id, true)
         }
     }
+
+    // Handling outside clicks and escape key to stop editing.
+	function handlePossibleOutsideClick(event: MouseEvent) {
+		if (event.target !== notesContainer && !notesContainer.contains(event.target as Node)) {
+			editable = false
+		}
+	}
+    function handleEscape(event: KeyboardEvent) {
+        if (event.key === "Escape") editable = false
+    }
 </script>
 
 
-<main>
+<svelte:body
+    on:click={handlePossibleOutsideClick}
+    on:keyup={handleEscape}
+/>
+
+
+<div class="notes-viewer">
     <h4>{title}</h4>
 
     <div
@@ -40,28 +58,30 @@
         ADD NOTES
     </div>
 
-    <div class="editable-toggle {editable ? "toggled": ""}">
-        Editable
-        <Toggle
-            bind:toggled={editable}
-        />
+    <!-- Container to hold either Note display or Note editor. -->
+    <div
+        class="notes-container"
+        bind:this={notesContainer}
+        on:dblclick={() => {editable = true}}
+    >
+        <!-- Note editor. -->
+        {#if editable}
+            <NotesEditor
+                {noteText}
+            />
+        <!-- Note display. -->
+        {:else}
+            <div class="notes-display">
+                {@html noteText}
+            </div>
+        {/if}
     </div>
-
-    {#if editable}
-        <NotesEditor
-            {notesText}
-        />
-    {:else}
-        <div class="notes-display">
-            {@html notesText}
-        </div>
-    {/if}
     
-</main>
+</div>
 
 
 <style>
-    main {
+    .notes-viewer {
         outline: solid 1px lightgrey;
         outline-offset: -1px;
 
@@ -84,20 +104,11 @@
         margin: 0;
     }
 
-    .editable-toggle {
-        flex: 0 0;
+    .notes-container {
+        flex: 1 1;
 
         display: flex;
-        flex-direction: row;
-        justify-content: flex-end;
-        gap: 5px;
-
-        font-size: 0.75rem;
-        color: lightgrey;
-    }
-
-    .editable-toggle.toggled {
-        color: darkgrey;
+        flex-direction: column;
     }
 
     .notes-display {
