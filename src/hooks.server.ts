d07d7a6c640e1a74edc8377d6c13fd061315f3df
Michael Gravina <gravina.2@osu.edu>
2023-01-20 18:06:37 -0500

    Refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,34 +1,41 @@
 import type { Handle } from "@sveltejs/kit"
 import { parse } from "cookie"
 import { getDatabaseConnection } from "$lib/db/connection"
-//import { stringRepresentsInteger } from "$lib/shared/utility"
 
 
+/**
+ * Server hook handle.
+ * 
+ * All requests to the server are first passed through this method, which sets
+ * up a connection to the database, specifying the appropriate file either from
+ * a cookie or from a passed parameter.
+ * @param param0 - An object passed by the framework containing the RequestEvent and a method to resolve it into a response.
+ * @returns - The response from the server to the client.
+ */
 export const handle: Handle = async ({ event, resolve }) => {
 
-	const cookies = parse(event.request.headers.get("cookie") || "")
-	const graphName = cookies.graphName || null
-	/*const pThingId =
-		cookies.pThingId && stringRepresentsInteger(cookies.pThingId) ? parseInt(cookies.pThingId) :
-		null*/
+	// If the request was to the database API,
+	if ( event.url.pathname.startsWith("/api/db/") ) {
 
+		// Retrieve the name of the Graph file from the cookies.
+		const cookies = parse(event.request.headers.get("cookie") || "")
+		const graphName = cookies.graphName || null
 
-	if (graphName) {
-		
-		if ( /*event.request.method === "GET" &&*/ event.url.pathname.startsWith("/api/db/") ) {
-			
-			await getDatabaseConnection(graphName)
-	
-		} else if ( event.request.method === "POST" && event.url.pathname.startsWith("/api/file/createGraph") ) {
-	
-			const body = await event.request.clone().json()
-			await getDatabaseConnection(body.newGraphName, null, true)
-	
-		}
+		// Get a database connection to that Graph file.
+		if (graphName) await getDatabaseConnection(graphName)			
+
+	// Else, if the request was sent to the create-Graph API,
+	} else if ( event.request.method === "POST" && event.url.pathname.startsWith("/api/file/createGraph") ) {
+
+		// Retrieve the name of the Graph file from the request body.
+		const body = await event.request.clone().json()
+
+		// Create that Graph file and get a database connection to it.
+		await getDatabaseConnection(body.newGraphName, null, true)
 
 	}
 
+	// Resolve a response from the event, and return it to the client.
 	const response = await resolve(event)
-	
 	return response
 }
