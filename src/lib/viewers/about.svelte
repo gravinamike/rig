9f34d6d99c1a50121d8404722a2c9104fa79e9a1
Michael Gravina <gravina.2@osu.edu>
2023-01-30 16:25:10 -0500

    Improved visual appearance of About menu

--- src/lib/viewers/about.svelte
+++ src/lib/viewers/about.svelte
@@ -17,31 +17,36 @@
     }
 </script>
 
-<div class="about-menu">
-    <h4>About</h4>
-    
-    <div>
-        <p>
-            This is Mike Gravina's attempt at building a general-purpose medium
-            for depicting how things are related to each other.
-        </p>
-        <p>
-            "Rig" is intended to overcome some of the limitations of
-            traditional, linear text. In particular, it can be difficult to
-            capture all the details of very complex systems when the content of
-            a model has to be arranged as a single long sequence, one section
-            after the other.
-        </p>
-        <p>
-            In Rig, content can be connected in many different ways, across a
-            large number of dimensions. The hope is that, together with
-            traditional forms of text, this can be useful for dealing with many
-            complex issues that are difficult to discuss with text alone.
-        </p>
-        <p>
-            Rig is in very early development. Watch for new features and bugfixes
-            in the future. And if you have any suggestions, let Mike know!
-        </p>
+<div class="about-menu">    
+    <div class="content">
+        <h4>About</h4>
+
+        <div>
+            <p>
+                This is an early version of an application by Mike Gravina called
+                Rig. It's focused on "Graphs", which are diagrams of the
+                connections between different things.
+            </p>
+            <p>
+                I hope Rig might serve to stimulate conversation around the form
+                of digital media. In particular, I'd like to highlight the fact
+                that it can be difficult to capture all the nuance of more complex
+                topics when content has to be arranged in linear sequence, one
+                section after another.
+                
+            </p>
+            <p>
+                In Rig, content can be connected in many different ways, across a
+                large number of "dimensions". My hope is that Rig can serve as
+                an evolving model of new conventions for representing knowledge -
+                conventions that build on the traditional linear form with new
+                tools for constructing rich and flexible models of the world.
+            </p>
+            <p>
+                Rig is in very early development. Watch for new features and bugfixes
+                in the future. And if you have any suggestions, let Mike know!
+            </p>
+        </div>
     </div>
 
     <div class="secret" on:click={displaySecret} on:keydown={()=>{}}>
@@ -51,17 +56,13 @@
   
 <style>
     .about-menu {
-        outline: solid 1px lightgrey;
-        outline-offset: -1px;
-
         box-sizing: border-box;
         height: 100%;
-        background-color: #fafafa;
+        background-color: #E8E8E8;
 
         display: flex;
         flex-direction: column;
-        padding: 0.75rem;
-        gap: 1.25rem;
+        padding: 0.5rem;
 
         overflow-x: hidden;
         overflow-y: auto;
@@ -72,6 +73,23 @@
         margin: 0;
 
         text-align: center;
+    }
+
+    .content {
+        flex: 1 1 0;
+        border-radius: 5px;
+        background-color: white;
+
+        display: flex;
+        flex-direction: column;
+
+        padding: 0.5rem 1rem 0.5rem 1rem;
+        gap: 1rem;
+    }
+
+    p {
+        font-size: 0.9rem;
+        text-indent: 1rem;
     }
 
     .secret {
