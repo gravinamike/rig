9aaba15ec3727912947899e2ab9a66a31e838eeb
Michael Gravina <gravina.2@osu.edu>
2023-05-16 22:01:52 -0400

    Added logic to forbid API endpoints if the client is not authorized to access them

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,6 @@
-import type { Handle } from "@sveltejs/kit"
+import type { Handle, RequestEvent } from "@sveltejs/kit"
 import { parse } from "cookie"
+import { error } from "@sveltejs/kit"
 import { getSessionById } from "$lib/server/auth"
 import { getDatabaseConnection } from "$lib/server/db/connection"
 import { retrieveSessionSpecificCookie } from "$lib/db/sessionSpecificFetch"
@@ -16,7 +17,7 @@ import { retrieveSessionSpecificCookie } from "$lib/db/sessionSpecificFetch"
  */
 export const handle: Handle = async ({ event, resolve }) => {
 
-	/* Identify user. */
+	/** Identify user. */
 
 	// Retrieve cookies from the request headers.
     const cookies = parse(event.request.headers.get("cookie") || "")
@@ -36,24 +37,29 @@ export const handle: Handle = async ({ event, resolve }) => {
 		event.locals.user = null
 	}
 
+	
+	/** If the request is forbidden, throw a 403 error. */
+	if (await requestIsForbidden(event)) throw error(403, "You don't have permission to access this resource.")
 
-	/* Get database connection. */
+
+	/** Get database connection. */
 
 	// If the request was to the database API,
 	if ( event.url.pathname.startsWith("/api/db/") ) {
 
 		// Retrieve the name of the Graph file from the cookies.
 		const graphName = retrieveSessionSpecificCookie(event.request, "graphName")
-		
+
 		// Get a database connection to that Graph file.
-		if (graphName) await getDatabaseConnection(graphName)			
+		if (graphName) {
+			await getDatabaseConnection(graphName)
+		}	
 
 	// Else, if the request was sent to the create-Graph API,
 	} else if ( event.request.method === "POST" && event.url.pathname.startsWith("/api/file/createGraph") ) {
-		// Retrieve the name of the Graph file from the request body.
-		const body = await event.request.clone().json()
 
 		// Create that Graph file and get a database connection to it.
+		const body = await event.request.clone().json()
 		await getDatabaseConnection(`${body.username || "all"}/${body.newGraphName}`, null, true)
 
 	}
@@ -62,4 +68,94 @@ export const handle: Handle = async ({ event, resolve }) => {
 	/* Resolve a response from the event, and return it to the client. */
 	const response = await resolve(event)
 	return response
+}
+
+
+/**
+ * Request-is-forbidden method.
+ * Determines if a request event should return a 404 (Forbidden) error instead
+ * of the regular response.
+ * 
+ * @param event - The request event.
+ * @returns - Boolean indicating whether or not the request it forbidden.
+ */
+async function requestIsForbidden(
+	event: RequestEvent<Partial<Record<string, string>>, string | null>
+) {
+	let requestIsForbidden = false
+
+	// Array of paths that are relative to a specific Graph and should be
+	// forbidden when the user is not authorized for that Graph.
+	const graphRestrictedPaths = [
+		"/api/db/graphConstructs",
+		"/api/db/graphFile",
+		"/api/db/graphManipulation",
+		"/api/file/attachmentsFolder-",
+		"/api/file/graphConfig"
+	]
+
+	// If the request URL is one of the Graph-restricted paths,
+	if ( graphRestrictedPaths.some(path => {return event.url.pathname.startsWith(path)}) ) {
+		// Get the Graph name and its owner's username from the session-
+		// specific cookie.
+		const graphName = retrieveSessionSpecificCookie(event.request, "graphName")
+		const usernameFromGraphName = graphName?.split("/")[0] || null
+
+		// If...
+		if (
+			// ...there is no Graph currently open...
+			!graphName
+			// ...or...
+			|| (
+				// ...there is a Graph currently open...
+				graphName
+				// ...and that Graph is neither a common Graph nor owned by the
+				// signed-in user,
+				&& !(
+					usernameFromGraphName === "all"
+					|| (
+						usernameFromGraphName
+						&& event.locals.user?.username === usernameFromGraphName
+					)
+				)
+			)
+		// The request is forbidden.
+		) requestIsForbidden = true
+	}
+
+	// If the request is for the create-Graph endpoint,
+	if ( event.url.pathname.startsWith("/api/file/createGraph") ) {
+		// Get a copy of the request body.
+		const body = await event.request.clone().json()
+
+		// If the to-be-created Graph is neither a common Graph nor one that
+		// belongs to the signed-in user, the request is forbidden.
+		if (
+			!(
+				body.username === null
+				|| (
+					body.username !== null
+					&& (event.locals.user?.username || null) === body.username
+				)
+			)
+		) requestIsForbidden = true
+	}
+
+	// If the request is for the Graph-folders-list endpoint and the user to
+	// check for is neither the common user nor the signed-in user, the request
+	// is forbidden.
+	if (
+		event.url.pathname.startsWith("/api/file/graphFolders-")
+		&& !(
+			event.params.username === "all"
+			|| (
+				event.params.username
+				&& (event.locals.user?.username || null) === event.params.username
+			)
+		)
+	) {
+		requestIsForbidden = true
+	}
+
+	return requestIsForbidden
 }
